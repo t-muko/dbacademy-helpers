1323a00dddeca913b156e9881f582a40d21d7c5f
Jacob D. Parr <me@jacobparr.com>
2021-08-10 15:58:47 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# import setuptools
+# from setuptools import find_packages
+
+# setuptools.setup(
+#     name="user-setup",
+#     version="1.0",
+#     packages=find_packages(),
+# )
+print("Hello from setup.py")
