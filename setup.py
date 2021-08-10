0b424eb8474d49d0bf6924697aed31b65ae50247
Jacob D. Parr <me@jacobparr.com>
2021-08-10 16:01:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 # Databricks notebook source
-# import setuptools
-# from setuptools import find_packages
+import setuptools
+from setuptools import find_packages
 
-# setuptools.setup(
-#     name="user-setup",
-#     version="1.0",
-#     packages=find_packages(),
-# )
-import sys
-print("Hello from setup.py")
-sys.exit(0)
+setup_var = "moo"
+
+setuptools.setup(
+    name="user-setup",
+    version="1.0",
+    packages=find_packages(),
+)
