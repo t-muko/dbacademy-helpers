61341b362305adf9dc7a4a783b73a11398f7a1b0
Jacob D. Parr <me@jacobparr.com>
2022-01-21 12:22:29 -0600

    x

--- dbacademy/helpers/__init__.py
+++ dbacademy/helpers/__init__.py
@@ -233,4 +233,4 @@ class DBAcademyHelper:
     if not silent: print(f"""\nThe install of the datasets completed successfully.""")    
   
 DBAcademy = DBAcademyHelper()
-DAH = DBAcademy
+dah = DBAcademy
